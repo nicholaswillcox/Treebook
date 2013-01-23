f6b38247dbb4a79d4c3a4c5d1ec2dd1c466b94a4
Nicholas B. Willcox <nicholas.b.willcox@gmail.com>
2013-01-22 20:49:04 -0600

    updated frontend for the new authentication system
    Added simple_form
    updated several forms to bootstrap/simple_form
    added login/logout/register/profile links to our nav
    Added a collection select to our status form

--- db/migrate/20130123004420_add_user_id_to_statuses.rb
+++ db/migrate/20130123004420_add_user_id_to_statuses.rb
@@ -3,7 +3,7 @@ class AddUserIdToStatuses < ActiveRecord::Migration
 
   	add_column :statuses, :user_id, :integer
   	add_index :statuses, :user_id
-  	remove_column :statuses, :name
+  	remove_column :statuses, :name	
 
   end
 end
